b36cf8f0ae4f6aee7fd80bcc57ac117d14b4dccb
Tumi Mashigo <tumi.mashigo15@gmail.com>
2022-10-17 19:43:52 +0200

    Add files via upload

---
+++ USD-vs-CNY/USD-vs-CNY/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+/*
+ Create a function that converts US dollars (USD) to Chinese Yuan (CNY) . The input is the amount of USD as an integer, 
+and the output should be a string that states the amount of Yuan followed by 'Chinese Yuan'
+
+Examples (Input -> Output)
+
+The conversion rate you should use is 6.75 CNY for every 1 USD. All numbers should be represented as a string with 2 decimal places. 
+(e.g. "21.00" NOT "21.0" or "21")
+*/
+
+public static class Kata
+{
+    public static string Usdcny(int usd)
+    {
+        decimal cny = 6.75m;
+        decimal result = usd * cny;
+        return ($"{result} Chinese Yuan");
+    }
+}
---
+++ What is between/What is between/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ Complete the function that takes two integers (a, b, where a < b)
+and return an array of all integers between the input parameters, including them.
+*/
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Kata
+{
+    public static int[] Between(int a, int b)
+    {
+        // your code here
+        var result = new List<int>();
+        for (var i = a; i <= b; i++)
+        {
+            result.Add(i);
+        }
+        return result.ToArray();
+    }
+}
+
+
