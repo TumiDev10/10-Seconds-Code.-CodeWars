7da585de137a18daf1a617fa6b2dd63f9ffb45ee
Tumi Mashigo <tumi.mashigo15@gmail.com>
2022-10-20 18:45:57 +0200

    Add files via upload

---
+++ Simple remove duplicates/Simple remove duplicates/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ Remove the duplicates from a list of integers, keeping the last ( rightmost ) occurrence of each element.
+
+Example:
+For input: [3, 4, 4, 3, 6, 3]
+
+remove the 3 at index 0
+remove the 4 at index 1
+remove the 3 at index 3
+Expected output: [4, 6, 3]
+
+More examples can be found in the test cases.
+
+Good luck!
+ */
+
+using System;
+using System.Linq;
+public class Solution
+{
+    public static int[] solve(int[] arr)
+    {
+
+        return arr.Reverse().Distinct().Reverse().ToArray();
+    }
+}
---
+++ Switcheroo/Switcheroo/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ Given a string made up of letters a, b, and/or c, switch the position of letters a and b 
+(change a to b and vice versa). Leave any incidence of c untouched.
+
+Example:
+
+'acb' --> 'bca'
+'aabacbaa' --> 'bbabcabb'
+ */
+
+using System;
+using System.Linq;
+public class Kata
+{
+    public static string Switcheroo(string x)
+    {
+        var letter = x.ToCharArray();
+        for (int i = 0; i < x.Length; i++)
+        {
+            if (letter[i] == 'a')
+                letter[i] = 'b';
+            else if (letter[i] == 'b')
+                letter[i] = 'a';
+        }
+        return new string(letter);
+    }
+}
